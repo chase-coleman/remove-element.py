73271eb976b448b3f31088f5d8e5f0cd66ec5872
YOUR_NAME <YOUR_EMAIL>
2025-02-24 22:56:09 -0600

    Created function to remove duplicated integers in a list (from Leetcode). Has 2 methods, a loop and a filter/lambda method.

---
+++ remove_element.py
@@ -0,0 +1,51 @@
+""""
+Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. 
+The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+
+Change the array nums such that the first k elements of nums contain the elements which are not equal to val. 
+The remaining elements of nums are not important as well as the size of nums.
+Return k.
+"""
+# k will be equal to the length of the array after removing the given value
+# loop through the array, and if it matches the current index, remove it
+class Solution(object):
+    def removeElement(self, nums, val):
+        print(len(nums))
+        if len(nums) > 1:
+            for num in nums[::-1]:
+                if num == val:
+                    nums.remove(num)  
+            k = len(nums)
+            print(k)
+            return k
+        elif len(nums) == 1 and nums[0] == val:
+            nums.pop()
+            k = len(nums)
+            print(k)
+            return k
+        elif len(nums) == 1 and nums[0] != val:
+            k = len(nums)
+            print(k)
+            return k
+    # This one fails Leetcode, because Leetcode wants the list modified in-place, and this creates a new list
+    def filterFunc(self, nums, val):
+        print(len(nums))
+        new_nums = list(filter(lambda x : x != val, nums))
+        k = len(new_nums)
+        print(k)
+        # print(new_nums)
+
+solution = Solution()
+# solution.removeElement([12, 57, 33, 84, 19, 45, 67, 23, 12, 88,35, 92, 45, 33, 72, 19, 57, 84, 63, 27,45, 12, 57, 84, 19, 33, 68, 92, 45, 72,5, 33, 88, 45, 19, 23, 57, 12, 45, 77,63, 33, 57, 84, 19, 45, 67, 23, 12, 92,33, 88, 63, 45, 19, 57, 84, 33, 12, 45,67, 33, 12, 72, 45, 84, 19, 57, 33, 92,45, 63, 19, 33, 45, 57, 12, 84, 33, 45,19, 63, 33, 45, 57, 12, 84, 92, 45, 33,19, 27, 63, 72, 33, 45, 12, 19, 57, 88], 57)
+# solution.filterFunc([24, 24, 24, 24, 24, 24, 24, 24,24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24,24, 24, 24, 24, 24], 24)
+# solution.removeElement([24, 24, 24, 24, 24, 24, 24, 24,24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24,24, 24, 24, 24, 24], 24)
+# solution.filterFunc([], 2)
+# solution.removeElement([1,2,3,4,5,6,7,8,9], 0)
+
+        
+
+
+
+
